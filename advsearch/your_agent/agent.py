abd2f709f947d36f7cf910ca97790d6d178260b9
Brenda Paetzoldt Silva <brendapaetzoldt@yahoo.com.br>
2022-06-16 18:45:25 -0300

    inicio de um exemplo
    ainda não está funcionando

--- advsearch/your_agent/agent.py
+++ advsearch/your_agent/agent.py
@@ -1,5 +1,7 @@
 import random
 import sys
+import time
+import copy
 
 # Voce pode criar funcoes auxiliares neste arquivo
 # e tambem modulos auxiliares neste pacote.
@@ -7,16 +9,59 @@ import sys
 # Nao esqueca de renomear 'your_agent' com o nome
 # do seu agente.
 
+# https://github.com/dhconnelly/paip-python/blob/907abe33650afb08995df4874a5eb1323826915c/paip/othello.py
 
-def make_move(the_board, color):
-    """
-    Returns an Othello move
-    :param the_board: a board.Board object with the current game state
-    :param color: a character indicating the color to make the move ('B' or 'W')
-    :return: (int, int) tuple with x, y indexes of the move (remember: 0 is the first row/column)
-    """
-    # o codigo abaixo apenas retorna um movimento aleatorio valido para
-    # a primeira jogada com as pretas.
-    # Remova-o e coloque a sua implementacao da poda alpha-beta
-    return random.choice([(2, 3), (4, 5), (5, 4), (3, 2)])
+def alphabeta(player, board, alpha, beta, depth, evaluate):
+ 
+    if depth == 0:
+        return evaluate(player, board), None
+
+    def value(board, alpha, beta):
+        # Like in `minimax`, the value of a board is the opposite of its value
+        # to the opponent.  We pass in `-beta` and `-alpha` as the alpha and
+        # beta values, respectively, for the opponent, since `alpha` represents
+        # the best score we know we can achieve and is therefore the worst score
+        # achievable by the opponent.  Similarly, `beta` is the worst score that
+        # our opponent can hold us to, so it is the best score that they can
+        # achieve.
+        return -alphabeta(opponent(player), board, -beta, -alpha, depth-1, evaluate)[0]
+    
+    moves = legal_moves(player, board)
+    if not moves:
+        if not any_legal_move(opponent(player), board):
+            return final_value(player, board), None
+        return value(board, alpha, beta), None
+    
+    best_move = moves[0]
+    for move in moves:
+        if alpha >= beta:
+            # If one of the legal moves leads to a better score than beta, then
+            # the opponent will avoid this branch, so we can quit looking.
+            break
+        val = value(make_move(move, player, list(board)), alpha, beta)
+        if val > alpha:
+            # If one of the moves leads to a better score than the current best
+            # achievable score, then replace it with this one.
+            alpha = val
+            best_move = move
+    return alpha, best_move
+
+def alphabeta_searcher(depth, evaluate):
+    def strategy(player, board):
+        return alphabeta(player, board, MIN_VALUE, MAX_VALUE, depth, evaluate)[1]
+    return strategy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
